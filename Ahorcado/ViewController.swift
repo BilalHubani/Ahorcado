06d20702b38b2615cb135a9134b396c4eadab744
BilalHubani <bilal.hubani@gmail.com>
2017-02-06 19:32:02 +0100

    initial commit

--- Ahorcado/ViewController.swift
+++ Ahorcado/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var words = ["charmander","pikachu","greninja","bulbasaur","squirtle","arceus"]
+    var images = [#imageLiteral(resourceName: "lisa_cuello.png"), #imageLiteral(resourceName: "lisa_brazo.png"), #imageLiteral(resourceName: "lisa_brazo.png"), #imageLiteral(resourceName: "lisa_cuerpo.png"), #imageLiteral(resourceName: "lisa_pierna.png"), #imageLiteral(resourceName: "lisa_piernas.png"), #imageLiteral(resourceName: "lisa_full.png")]
+    var chars = Array<Character>()
+    var fails = 0
+    var chosenWord = [Character]()
+    
+    
+    
+    
 
+    @IBOutlet weak var word: UILabel!
+    @IBOutlet weak var letter: UITextField!
+    @IBOutlet weak var charsUsed: UILabel!
+    @IBOutlet weak var hanged: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let random = Int(arc4random_uniform(UInt32(words.count)))
+        chosenWord = Array(words[random].characters)
     }
 
     override func didReceiveMemoryWarning() {
